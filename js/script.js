7a5a72132804e0a4078b4b39c4103f73835701eb
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-07-17 23:36:29 +0300

    vazifaa

---
+++ js/script.js
@@ -0,0 +1,315 @@
+// IF misollar
+// 1-misol
+
+
+// let num = +prompt("sonni kiriting")
+// if(num > 0) {
+//     num++;
+// }
+
+// console.log(num)
+
+
+// 2-misol
+
+// let num = +prompt("sonni kiriting")
+// if(num > 0) {
+//     num++;
+// } else {
+//     num -= 2;
+// }
+
+// console.log(num)
+
+
+
+// 3-misol
+
+// let = +prompt("musbat va manfiy sonlarni chiqaradi")
+// let number;  // Example input
+
+// if (number > 0) {
+//   number += 1;
+// } else if (number < 0) {
+//   number -= 2;
+// } else {
+// number;
+// }
+
+// console.log(number);
+
+// 4-misol
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let max ;
+// if(num1 > 0) {
+//     max = num1;
+// } if(num2 > 0) {
+//     max = num2;
+// }if(num3 > 0) {
+//     max = num3;
+// }
+
+// console.log(max)
+
+
+
+
+
+// 6-misol
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let max ;
+// if(num1 > num2) {
+//     max = num1
+// } if(num1<num2) {
+//     max = num2
+// }
+
+// console.log(max)
+
+// 7-misol
+
+
+    // let num1 = +prompt("1-sonni kiriting");
+    // let num2 = +prompt("2-sonni kiriting");
+    // let min ;
+    // if(num1 > num2) {
+    //     min = num2
+    // } if(num1<num2) {
+    //     min = num1
+    // }
+
+    // console.log(min)
+
+
+
+    // 8-misol
+
+    // let num1 = +prompt("1-sonni kiriting");
+    // let num2 = +prompt("2-sonni kiriting");
+    // let min ;
+    // let max ;
+    // if(num1 > num2) {
+    //     min = num2
+    //     max = num1
+    // } if(num1<num2) {
+    //     min = num1
+    //     max = num2
+    // }
+
+    // console.log(min, max)
+
+
+    // 10-misol
+
+    // let a = +prompt("1-sonni kiriting...");
+    // let b = +prompt("2-sonni kiriting...");
+    // let res ;
+    // if(a != b) {
+    //     res = a + b ;
+    // } 
+    // if(a == b) {
+    //     res = 0
+    // }
+
+    // console.log(res)
+
+    // 11-misol
+
+    // let a = +prompt("1-sonni kiriting...");
+    // let b = +prompt("2-sonni kiriting...");
+    // let res ;
+    // let max ;
+    // if(a != b) {
+    //     if(a > b) {
+    //        max = a
+    //         } if(a < b) {
+    //             max = b
+    //         }
+    // } 
+    // if(a == b) {
+    //     res = 0
+    // }
+
+    // console.log(res, max)
+
+// 12-misol
+
+
+// let num1 = +prompt("1-sonni kiriting.")
+// let num2 = +prompt("2-sonni kiriting.")
+// let num3 = +prompt("3-sonni kiriting.")
+
+// let smallest = num1; 
+
+// if (num2 < smallest) {
+//     smallest = num2;  
+// }
+
+// if (num3 < smallest) {
+//     smallest = num3;  
+// }
+
+// console.log("Eng kichik son:", smallest);   
+
+
+
+
+    // 14-misol
+
+//     let num1 = +prompt("1-sonni kiriting.")
+// let num2 = +prompt("2-sonni kiriting.")
+// let num3 = +prompt("3-sonni kiriting.")
+
+// let smallest = num1; 
+// let engKatta = num1;
+// if (num2 < smallest) {
+//     smallest = num2;  
+// }
+
+// if (num3 < smallest) {
+//     smallest = num3;  
+// }
+// if(num1 > num2) {
+//     max = num1
+// } if(num1<num2) {
+//     max = num2
+// }
+
+
+// if (num2 > engKatta) {
+//     engKatta = num2;
+// }
+
+// if (num3 > engKatta) {
+//     engKatta = num3;
+// }
+
+// console.log("Eng katta son: " + engKatta);
+
+
+
+// console.log("Eng kichik son:", smallest);   
+
+
+
+
+
+// Boolean vazifalar
+
+// 1-misol
+// let A = +prompt("Sonni kiriting..")
+// let =   res = false;
+
+// if (A > 0) {
+//     res = true;
+// }
+
+// console.log(res);
+
+
+// 2-misol
+
+// let A = +prompt("Sonni kiriting..")
+// let =   res = false;
+
+// if (A % 2 == 1) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 3-misol
+
+// let A = +prompt("Sonni kiriting..")
+// let =   res = false;
+
+// if (A % 2 == 0) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 4-misol
+
+
+// let A = +prompt("Sonni kiriting..")
+// let B = +prompt("Sonni kiriting..")
+// let =   res = false;
+
+// if (A > 2 && B <= 3) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 5-misol
+
+// let A = +prompt("Sonni kiriting..")
+// let B = +prompt("Sonni kiriting..")
+// let =   res = false;
+
+// if (A >= 0 || B <= -2) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 6-misol
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+// let C = +prompt("3-Sonni kiriting..")
+// let =   res = false;
+
+// if (A <= B <= C ) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 8-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A % 2 == 1 && B % 2 == 1 ) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 9-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A % 2 == 1 && B % 2 == 0 || A % 2 == 0 &&  B % 2 == 1 ) {
+//     res = true;
+// }
+
+// console.log(res);
+  
+
+// 10-misol
+
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A % 2 == 1 && B % 2 == 0 || A % 2 == 0 &&  B % 2 == 1 ) {
+//     res = true;
+// }
+
+// console.log(res);
+
+   
